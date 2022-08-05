e5a62f830c2d7ecdfb331ae97361f8b99dd1932f
Easha <vasubhatt60@gmail.com>
2022-08-04 22:20:33 -0500

    added variable

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -2,13 +2,13 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
 @TeleOp()
 
 public class HelloWorld extends OpMode {
+    String name = "user";
     @Override
     public void init() {
-        telemetry.addData("Hello", "World");
+        telemetry.addData("Hello", name);
     }
     @Override
     public void loop() {
